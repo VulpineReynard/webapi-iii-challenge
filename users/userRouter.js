46030e1a7e149758dced9474117b51ea20fb5dc3
VulpineReynard <milare8@gmail.com>
2019-09-05 18:18:56 -0400

    validation works

--- users/userRouter.js
+++ users/userRouter.js
@@ -33,24 +33,39 @@ router.route("/")
 router.route("/:id")
 // get specific user
 .get(validateUserId, function idGetController(req, res){
+
   userHelper.getById(req.params.id)
     .then(user => {
       res.status(200).send(user);
     })
+    .catch(err => {
+      res.status(400).send({ message: "Something went wrong." })
+    })
+
 })
 // delete specific user
 .delete(validateUserId, function idDeleteController(req, res){
+
   userHelper.remove(req.params.id)
     .then(data => {
       res.status(200).send('Resourse deleted successfully.');
     })
+    .catch(err => {
+      res.status(400).send({ message: "Something went wrong." })
+    })
+
 })
 // update specific user
 .put(validateUserId, function idPutController(req, res) {
+
   userHelper.update(req.params.id, req.body)
     .then(count => {
       res.status(200).send('Updated Sucessfully.');
     })
+    .catch(err => {
+      res.status(400).send({ message: "Something went wrong." })
+    })
+
 })
 
 
@@ -58,43 +73,55 @@ router.route("/:id")
 router.route("/:id/posts")
 // get user's posts
 .get(validateUserId, function postWithIdGetController(req, res) {
+
   postHelper.get()
     .then(posts => {
       let targetPosts = posts.filter(post => post.user_id === 1)
       res.status(200).send(targetPosts);
     })
+    .catch(err => {
+      res.status(400).send({ message: "Something went wrong." })
+    })
+
 })
 // create a post
-.post(validateUserId, validatePost, function postWithIdPostController(req, res) {
-  let newPost = req.body;
-  newPost.user_id = req.user;
-  console.log(newPost);
-  postHelper.insert(newPost)
+.post(
+  validateUserId, 
+  validatePost, 
+  function postWithIdPostController(req, res) {
+
+  req.body.user_id = req.user;
+  postHelper.insert(req.body)
     .then(returned => {
       res.status(201).send(returned);
     })
+    .catch(err => {
+      res.status(400).send({ message: "Something went wrong." })
+    })
+
 })
 
 // <----- CUSTOM MIDDLEWARE ----->
 
 // validates the user id given is valid
 function validateUserId(req, res, next) {
-  let id = req.params.id;
-  userHelper.getById(id)
+  const { id } = req.params;
+  userHelper.getById(id)  
     .then(user => {
-      req.user = id;
-      next();
+      req.user = user;
     })
     .catch(err => {
-      res.status(400).send('Invalid ID');
+      res.status(400).send("Invalid User Id")
     })
+    next();
 };
 
 // check new user request object
 function validateUser(req, res, next) {
-  if (!req.body) {
+  const { name } = req.body;
+  if (isEmpty(req.body)) {
     res.status(400).send({ message: "Missing user data" })
-  } else if (req.body && !req.body.name) {
+  } else if (req.body && !name) {
     res.status(400).send({ message: "Missing required name field" })
   } else {
     next();
@@ -102,7 +129,22 @@ function validateUser(req, res, next) {
 };
 
 function validatePost(req, res, next) {
-  next();
+  const { text } = req.body;
+  if (isEmpty(req.body)) {
+    res.status(400).send({ message: "Missing user data" })
+  } else if (req.body && !text) {
+    res.status(400).send({ message: "Missing required name field" })
+  } else {
+    next();
+  }
 };
+
+function isEmpty(obj) {
+  for(var key in obj) {
+    if(obj.hasOwnProperty(key))
+      return false;
+  }
+  return true;
+}
 
 module.exports = router;
